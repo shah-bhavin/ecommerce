c4ea27b109661818ba36de3be2a36afea9c83af6
shah-bhavin <shahbhavin1402@gmail.com>
2026-03-21 11:16:31 +0530

    Fix test: use soft delete assertion

--- tests/Feature/Settings/ProfileUpdateTest.php
+++ tests/Feature/Settings/ProfileUpdateTest.php
@@ -57,7 +57,8 @@ test('user can delete their account', function () {
         ->assertHasNoErrors()
         ->assertRedirect('/');
 
-    expect($user->fresh())->toBeNull();
+    //expect($user->fresh())->toBeNull();
+    $this->assertSoftDeleted($user);
     expect(auth()->check())->toBeFalse();
 });
 
